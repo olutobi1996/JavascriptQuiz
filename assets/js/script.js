02d91550f6e7f9a8613b6a998256aace8a669c59
olutobi Adebayo <olutobi1996@icloud.com>
2024-11-25 16:37:47 +0000

    quiz variables created js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let timeLeft = document.querySelector(".time-left");
+let quizContainer = document.getElementById("container");
+let nextBtn = document.getElementById("next-button");
+let countOfQuestion = document.querySelector(".number-of-question");
+let displayContainer = document.getElementById("display-container");
+let scoreContainer = document.querySelector(".score-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector(".start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0;
+let count = 11;
+let countdown;
